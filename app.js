95029fc208c2baa706846c3d2e1ae1bd3646a860
Juan Martinez <juan.martinez43@udea.edu.co>
2022-12-29 21:49:18 +0000

    Change in the About me, also project

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
-
+//Function to create a display button.
+function myFunction(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    if(dots.style.display == 'none'){
+        dots.style.display = 'inline'
+        btnText.innerHTML = 'Read More'
+        moreText.style.display = 'none'
+    }else{
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Read Less';
+        moreText.style.display = 'inline'
+    }
+}
 // Function to asign the class and activate the sections of the website. 
 function PageTransitions() {
     //Btn click active class.
